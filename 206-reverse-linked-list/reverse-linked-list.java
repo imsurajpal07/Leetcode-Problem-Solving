adf32e466a48795a3db8c5b807f339d021f7f97f
Suraj Pal <31159535+imsurajpal07@users.noreply.github.com>
2025-09-26 18:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (11.47%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,16 +9,36 @@
  * }
  */
 class Solution {
+    // public ListNode reverseList(ListNode head) {
+    //     ListNode prev = null;
+    //     ListNode curr = head;
+    //     while (curr != null) {
+    //         ListNode next = curr.next;
+    //         curr.next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
+    //     return prev;
+    // }
+
+    //  public ListNode reverseList(ListNode head) {
+    //     return reverseTailRecuursive(head, null);
+    // }
+
+    // private static ListNode reverseTailRecuursive(ListNode head, ListNode prev) {
+    //     if (head == null)
+    //         return prev;
+    //     ListNode next = head.next;
+    //     head.next = prev;
+    //     return reverseTailRecuursive(next, head);
+    // }
+
     public ListNode reverseList(ListNode head) {
-        ListNode curr = head;
-        ListNode prev = null;
-        while(curr != null)
-        {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+        if (head == null || head.next == null)
+            return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 }
